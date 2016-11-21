c8a525a2d9b9e01cc174541ff4c2386955edc645
damyana79 <damyana@gmx.net>
2016-11-21 05:11:58 +0100

    changed WriteProcessedOutput, added writer for verbKeys - verbAspect

--- src/WriteProcessedOutput.java
+++ src/WriteProcessedOutput.java
@@ -24,13 +24,16 @@ public class WriteProcessedOutput {
      * @param outputFilename_1
      * @param outputFilename_2
      */
-    public static void writeSelectedSentences(String outputFilename_1, String outputFilename_2) {
+    public static void writeSelectedSentences(String outputFilename_1, String outputFilename_2, String verbkeysAspect) {
         emptyFile(outputFilename_1);
         emptyFile(outputFilename_2);
+        emptyFile(verbkeysAspect);
         Map<String, List<OutputVerbData>> outputVerbDataDictionary = OutputVerbDataDictionary.getInstance().outputVerbDataDictionary;
         Set<String> verbKeys = outputVerbDataDictionary.keySet();
         Path path_1 = Paths.get(outputFilename_1);
         Path path_2 = Paths.get(outputFilename_2);
+        Path path_3 = Paths.get(verbkeysAspect);
+
         try (BufferedWriter bufferedWriter_1 = Files.newBufferedWriter(path_1,
                 StandardCharsets.UTF_8, StandardOpenOption.CREATE,
                 StandardOpenOption.APPEND, StandardOpenOption.WRITE);
@@ -39,7 +42,12 @@ public class WriteProcessedOutput {
              BufferedWriter bufferedWriter_2 = Files.newBufferedWriter(path_2,
                      StandardCharsets.UTF_8, StandardOpenOption.CREATE,
                      StandardOpenOption.APPEND, StandardOpenOption.WRITE);
-             CSVWriter writer_2 = new CSVWriter(bufferedWriter_2);) {
+             CSVWriter writer_2 = new CSVWriter(bufferedWriter_2);
+
+             BufferedWriter bufferedWriter_3 = Files.newBufferedWriter(path_3,
+                     StandardCharsets.UTF_8, StandardOpenOption.CREATE,
+                     StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+             CSVWriter writer_3 = new CSVWriter(bufferedWriter_3);) {
 
             for (String verb : verbKeys) {
                 List<OutputVerbData> outputList = outputVerbDataDictionary.get(verb);
@@ -48,6 +56,8 @@ public class WriteProcessedOutput {
                     Verb firstOutputVerb = firstOutput.verb_en;
                     String[] verbData = new String[]{firstOutputVerb.infinitiv, firstOutputVerb.token, firstOutputVerb.aspect, firstOutput.fullSentence_en};
                     writer_1.writeNext(verbData);
+                    String[] verbAspect = new String[]{firstOutputVerb.infinitiv, firstOutputVerb.aspect};
+                    writer_3.writeNext(verbAspect);
                 }
                 if (outputList.size() > 1) {
                     OutputVerbData secondOutput = outputList.get(1);
@@ -74,14 +84,15 @@ public class WriteProcessedOutput {
         try (BufferedWriter writer = Files.newBufferedWriter(path,
                 StandardCharsets.UTF_8, StandardOpenOption.CREATE,
                 StandardOpenOption.APPEND, StandardOpenOption.WRITE);
-             BufferedWriter writer_2 = Files.newBufferedWriter(path_2,
+             BufferedWriter bufferedWriter_2 = Files.newBufferedWriter(path_2,
                      StandardCharsets.UTF_8, StandardOpenOption.CREATE,
-                     StandardOpenOption.APPEND, StandardOpenOption.WRITE)
-        ) {
+                     StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+             CSVWriter writer_2 = new CSVWriter(bufferedWriter_2);) {
 
             for (String verb : verbKeys) {
                 writer.append(verb + "\n");
-                writer_2.append(verb + " - " + outputVerbDataDictionary.get(verb).size() + "\n");
+                String[] verbOccurences = new String[]{verb, String.valueOf(outputVerbDataDictionary.get(verb).size())};
+                writer_2.writeNext(verbOccurences);
 
             }
         } catch (IOException e) {
@@ -131,10 +142,11 @@ public class WriteProcessedOutput {
         //filenames for writing
 
         String writeKeys = "processed_output/verb_keys.txt";
-        String writeKey_occurrenceNumber = "processed_output/verb_keys_occurrenceNumber.txt";
+        String writeKey_occurrenceNumber = "processed_output/verb_keys_occurrenceNumber.csv";
         //String writeKeys = "processed_output_big/verb_keys.txt";
         String writeSelected_1 = "processed_output/selected_1.csv";
         String writeSelected_2 = "processed_output/selected_2.csv";
+        String writeVerbAspect = "processed_output/verbKeyAspect.csv";
 
         //String writeFullOutput = "processed_output_big/full_output.txt"; // too big file, not really necessary
         //String writeSelected_1 = "processed_output_big/selected_3.csv";
@@ -142,8 +154,8 @@ public class WriteProcessedOutput {
 
         //object
         WriteProcessedOutput processedOutput = new WriteProcessedOutput();
-        writeVerbKeys(writeKeys, writeKey_occurrenceNumber);
-        //writeSelectedSentences(writeSelected_1, writeSelected_2);
+        //writeVerbKeys(writeKeys, writeKey_occurrenceNumber);
+        writeSelectedSentences(writeSelected_1, writeSelected_2, writeVerbAspect);
 
 
     }
